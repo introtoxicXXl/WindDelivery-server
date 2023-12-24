7d2bf098160dec03a9e503b7b9a238ac6be81caf
introtoxicXXl <minhajulabedin648@gmail.com>
2023-12-24 23:23:44 +0600

    single product done

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
 
         });
 
+        // single product 
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
