4aa40b29585057c799ffe3907adcc8c0b96bc628
introtoxicXXl <minhajulabedin648@gmail.com>
2023-12-26 21:45:57 +0600

    cookie parser and jwt token install

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(express.json())
 app.use(cors())
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dtrhla5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
